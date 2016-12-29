915302d5c02c97a7109e94209f5bc482c1ba8910
Logan <logan2299@yahoo.com>
2016-12-28 21:45:52 -0500

    Initial Commit

---
+++ SpudScript/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SpudScript
+//
+//  Created by Logan Pazol on 12/28/16.
+//  Copyright © 2016 Logan Pazol. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
